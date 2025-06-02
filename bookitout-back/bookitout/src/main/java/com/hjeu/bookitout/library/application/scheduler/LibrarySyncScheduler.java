2481aa2451702d4a79e0502e81a3e2bb1c5c6069
yuhyejin <hj_eu@naver.com>
2025-06-02 15:33:59 +0900

    feat: 도서관 api 매월 1일 자정 스케줄러 추가

--- bookitout-back/bookitout/src/main/java/com/hjeu/bookitout/library/application/scheduler/LibrarySyncScheduler.java
+++ bookitout-back/bookitout/src/main/java/com/hjeu/bookitout/library/application/scheduler/LibrarySyncScheduler.java
@@ -1,9 +1,12 @@
 package com.hjeu.bookitout.library.application.scheduler;
 
 import com.hjeu.bookitout.library.service.LibrarySyncService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
+@Slf4j
 @Component
 public class LibrarySyncScheduler {
 
@@ -12,5 +15,12 @@ public class LibrarySyncScheduler {
     @Autowired
     public LibrarySyncScheduler(LibrarySyncService librarySyncService) {
         this.librarySyncService = librarySyncService;
+    }
+
+    @Scheduled(cron = "0 0 0 1 * ?")    // 매월 1일 자정
+    public void autoSyncLibraryData() {
+        log.info("도서관 정보 스케줄러 시작");
+        librarySyncService.syncLibraryData();
+        log.info("도서관 정보 스케줄러 끝");
     }
 }
