c201187389e05aa82296bd04ff7ce4f10b6c0d8b
yuhyejin <hj_eu@naver.com>
2025-06-11 01:31:54 +0900

    fix: ESLint 에러 수정 - a 태그를 Link 컴포넌트로 변경

--- bookitout-front/src/components/BottomNav.tsx
+++ bookitout-front/src/components/BottomNav.tsx
@@ -1,26 +1,30 @@
-import React from "react";
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { FaHome, FaSearch, FaBook, FaUser } from 'react-icons/fa';
 
-const BottomNav = () => (
-  <nav className="fixed bottom-0 left-0 right-0 bg-white shadow-lg rounded-t-xl mx-4 lg:hidden">
-    <div className="flex justify-around">
-      <a href="#" className="flex flex-col items-center py-3 px-4 text-purple-600">
-        <i className="fas fa-home text-lg mb-1"></i>
-        <span className="text-xs">홈</span>
-      </a>
-      <a href="#" className="flex flex-col items-center py-3 px-4 text-gray-500">
-        <i className="fas fa-search text-lg mb-1"></i>
-        <span className="text-xs">검색</span>
-      </a>
-      <a href="#" className="flex flex-col items-center py-3 px-4 text-gray-500">
-        <i className="fas fa-heart text-lg mb-1"></i>
-        <span className="text-xs">즐겨찾기</span>
-      </a>
-      <a href="#" className="flex flex-col items-center py-3 px-4 text-gray-500">
-        <i className="fas fa-user text-lg mb-1"></i>
-        <span className="text-xs">마이페이지</span>
-      </a>
-    </div>
-  </nav>
-);
+const BottomNav = () => {
+  return (
+    <nav className="fixed bottom-0 left-0 right-0 bg-white border-t border-gray-200">
+      <div className="flex justify-around items-center h-16">
+        <Link to="/" className="flex flex-col items-center">
+          <FaHome className="text-xl" />
+          <span className="text-xs mt-1">홈</span>
+        </Link>
+        <Link to="/search" className="flex flex-col items-center">
+          <FaSearch className="text-xl" />
+          <span className="text-xs mt-1">검색</span>
+        </Link>
+        <Link to="/library" className="flex flex-col items-center">
+          <FaBook className="text-xl" />
+          <span className="text-xs mt-1">도서관</span>
+        </Link>
+        <Link to="/profile" className="flex flex-col items-center">
+          <FaUser className="text-xl" />
+          <span className="text-xs mt-1">프로필</span>
+        </Link>
+      </div>
+    </nav>
+  );
+};
 
 export default BottomNav;
